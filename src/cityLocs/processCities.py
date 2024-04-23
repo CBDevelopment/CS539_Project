2edc9a25539cae336e1edb17e6760b762ac270a5
Cutter Beck <cbeckdevelopment@gmail.com>
2024-04-23 13:52:16 -0400

    Add Voronoi tesselation

--- src/cityLocs/processCities.py
+++ src/cityLocs/processCities.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.spatial import Voronoi, voronoi_plot_2d
 
 # # https://simplemaps.com/data/world-cities
 # # TODO: Make sure to cite this source if using it
@@ -14,10 +15,19 @@ import pandas as pd
 # TODO: reference this source if using
 us_cities = pd.read_csv("uscities.csv")
 us_cities = us_cities.dropna()
-print(len(us_cities))
+continental_us_cities = us_cities[(us_cities['lat'] > 24.396308) & (us_cities['lat'] < 49.384358) & (us_cities['lng'] > -125.0) & (us_cities['lng'] < -66.93457)]
+# randomly remove 30% of cities
+continental_us_cities = continental_us_cities.sample(frac=0.2)
+print(len(continental_us_cities))
 
-plt.scatter(us_cities['lng'], us_cities['lat'])
-plt.xlabel("Longitude")
-plt.ylabel("Latitude")
-plt.title("US Cities")
+vor = Voronoi(continental_us_cities[['lng', 'lat']])
+voronoi_plot_2d(vor)
+plt.xlim(-125, -66)
+plt.ylim(24, 49)
 plt.show()
+
+# plt.scatter(continental_us_cities['lng'], continental_us_cities['lat'], s=1)
+# plt.xlabel("Longitude")
+# plt.ylabel("Latitude")
+# plt.title("US Cities")
+# plt.show()
